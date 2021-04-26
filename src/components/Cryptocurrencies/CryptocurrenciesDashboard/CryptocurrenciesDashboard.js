a7d8e837405c25aa39fca7a00af5c05c50cd963b
Anita Swieconek <anita.swieconek92@gmail.com>
2021-04-26 18:59:51 +0200

    added custom hook

--- src/components/Cryptocurrencies/CryptocurrenciesDashboard/CryptocurrenciesDashboard.js
+++ src/components/Cryptocurrencies/CryptocurrenciesDashboard/CryptocurrenciesDashboard.js
@@ -1,113 +1,17 @@
-import { useState, useEffect } from 'react';
-import axios from 'axios';
+import { useEffect } from 'react';
 import Header from '../../UI/Header/Header';
 import CryptocurrenciesList from '../CryptocurrenciesList/CryptocurrenciesList';
 import ObservedCurrencies from '../ObservedCurrencies/ObservedCurrencies';
 import Search from '../Search/Search';
 import { Route, Switch } from 'react-router-dom';
+import useCryptocurrencies from '../../../hooks/useCryptocurrencies';
 
 const CryptocurrenciesDasboard = () => {
-	const API_KEY = 'ab9b3876-c43d-425a-b9f8-3756e97bda52';
-	const [ cryptocurrencies, setCryptocurrencies ] = useState([]);
-
-	const fetchData = async () => {
-		try {
-			const responseCryptocurrencies = await axios.get(
-				`http://localhost:8080/https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?CMC_PRO_API_KEY=${API_KEY}&limit=50`
-			);
-
-			const cryptocurrenciesData = responseCryptocurrencies.data;
-
-			const ids_array = cryptocurrenciesData.data.map((cryptocurrency) => cryptocurrency.id);
-			const imagesToFetch = ids_array.join(',');
-
-			const responseIcons = await axios.get(
-				`http://localhost:8080/https://pro-api.coinmarketcap.com/v1/cryptocurrency/info?CMC_PRO_API_KEY=${API_KEY}&id=${imagesToFetch}`
-			);
-
-			const iconsData = responseIcons.data;
-
-			setCryptocurrencies(() => {
-				return cryptocurrenciesData.data.map((cryptocurrency) => {
-					cryptocurrency.icon = iconsData.data[cryptocurrency.id].logo;
-					cryptocurrency.isObserved = false;
-					return cryptocurrency;
-				});
-			});
-		} catch (error) {
-			console.log(error);
-		}
-	};
-
-	// const fetchData = async () => {
-	// 	try {
-	// 		const responseCryptocurrencies = await axios.get(
-	// 			`http://localhost:8080/https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?CMC_PRO_API_KEY=${API_KEY}&limit=50`
-	// 		);
-
-	// 		const cryptocurrenciesData = responseCryptocurrencies.data;
-
-	// 		const ids_array = cryptocurrenciesData.data.map((cryptocurrency) => cryptocurrency.id);
-	// 		const imagesToFetch = ids_array.join(',');
-
-	// 		const responseIcons = await axios.get(
-	// 			`http://localhost:8080/https://pro-api.coinmarketcap.com/v1/cryptocurrency/info?CMC_PRO_API_KEY=${API_KEY}&id=${imagesToFetch}`
-	// 		);
-
-	// 		const iconsData = responseIcons.data;
-
-	// 		console.log(cryptocurrenciesData.data);
-	// 		console.log(iconsData.data);
-
-	// 		return [ cryptocurrenciesData, iconsData ];
-	// 	} catch (error) {
-	// 		console.log(error);
-	// 	}
-	// };
-
-	// const cryptocurrenciesSetup = (cryptocurrenciesInfo, cryptocurrenciesIcons) => {
-	// 	setCryptocurrencies(() => {
-	// 		if (cryptocurrenciesInfo && cryptocurrenciesIcons) {
-	// 			return cryptocurrenciesInfo.data.map((cryptocurrency) => {
-	// 				cryptocurrency.icon = cryptocurrenciesIcons.data[cryptocurrency.id].logo;
-	// 				cryptocurrency.isObserved = false;
-	// 				return cryptocurrency;
-	// 			});
-	// 		}
-	// 	});
-	// };
+	const { cryptocurrencies, fetchData, handleObserved, handleRemoval, handleSearch } = useCryptocurrencies([]);
 
 	useEffect(() => {
 		fetchData();
-		// cryptocurrenciesSetup(fetchData[0], fetchData[1]);
 	}, []);
-
-	const handleObserved = (currencyId) => {
-		const cryptocurrenciesToObserve = cryptocurrencies.map(
-			(cryptocurrency) =>
-				cryptocurrency.id === currencyId ? { ...cryptocurrency, isObserved: true } : cryptocurrency
-		);
-		setCryptocurrencies(cryptocurrenciesToObserve);
-	};
-
-	const handleRemoval = (currencyId) => {
-		const cryptocurrenciesToRemoveFromObserved = cryptocurrencies.map(
-			(cryptocurrency) =>
-				cryptocurrency.id === currencyId ? { ...cryptocurrency, isObserved: false } : cryptocurrency
-		);
-		setCryptocurrencies(cryptocurrenciesToRemoveFromObserved);
-	};
-
-	const handleSearch = (searchedItem) => {
-		const filteredCryptocurrencies = cryptocurrencies.filter((currency) => {
-			return (
-				currency.name.toLowerCase().includes(searchedItem.toLowerCase()) ||
-				currency.symbol.toLowerCase().includes(searchedItem.toLowerCase())
-			);
-		});
-
-		setCryptocurrencies(filteredCryptocurrencies);
-	};
 
 	return (
 		<div>
